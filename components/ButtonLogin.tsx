bdcb31a73aa82cd48069ef61891a855da40aac47
Ilyas Seisov <ilyasseisov@gmail.com>
2025-02-17 12:22:29 +0500

    update

---
+++ components/ButtonLogin.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+export default function ButtonLogin() {
+  // hooks
+  // local variables
+  // functions
+  // return
+  return (
+    <>
+      <Link className='btn btn-primary' href='/dashboard'>
+        Dashboard
+      </Link>
+    </>
+  );
+}
---
+++ public/img/index.ts
@@ -0,0 +1,9 @@
+// file import
+import productDemo from './productDemo.jpeg';
+
+//export
+const images = {
+  productDemo,
+};
+
+export default images;
