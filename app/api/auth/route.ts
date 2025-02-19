b206422a0d42a3886f7b11172a87a9447005357d
Ilyas Seisov <ilyasseisov@gmail.com>
2025-02-19 12:17:17 +0500

    update

---
+++ app/api/auth/route.ts
@@ -0,0 +1,7 @@
+export function GET() {
+  return Response.json({ message: 'hello' });
+}
+
+export function POST() {
+  //
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import Image from 'next/image';
 import images from '@/public/img';
 
 export default function Home() {
+  //
+  console.log(process.env.MONGO_URI);
+  //
   return (
     <main>
       {/* HEADER */}
